99119691ebb1cd626161adea49997c0b53d5d377
Michal Piekarczyk <michal.piekarczyk@gmail.com>
2012-12-18 10:14:22 -0500

    added a line validator , also to main

--- main-simple.py
+++ main-simple.py
@@ -2,11 +2,9 @@
 
 import columnify
 import sys
+import lines_validate
 
 
-# line = "00:00 - 02:00 unk unknown"
-#  
-# line2= "23:10 - 00:00 fun Photoshop at starbucks"
 # delta, catgry = columnify.parseLineWTime(line2.split()) 
 # print "below line has length " , delta , " for category  " , catgry
 # print line2
@@ -15,6 +13,8 @@ import sys
 # columnify.parseDay()
 
 timesheet = "/Users/michal/Documents/timesheet parser/timesheet data/dec.1.2012.txt"
+
+lines_validate.validate_lines(timesheet)
 
 columnify.parse_timesheet(timesheet)
 
