555898c01359c6be34a120097c0eecb73e93b76a
Michal Piekarczyk <michal.piekarczyk@gmail.com>
2013-01-13 21:02:54 -0500

    added a settings file for easier global configurations

---
+++ settings.py
@@ -0,0 +1,17 @@
+GARBAGE_PREPENDING_SEQUENCES = [
+	r"\\cf0 (.*$)",
+	r"\\f0\\fs48 \\cf0 (.*$)",
+]
+YEAR = 2013
+
+
+
+ERROR_LOG = "error.log"
+
+
+
+REPORT_FILE = "reports/report_" 
+REPORT_FILE_CLEAN = "reports/report_clean_" 
+DIDNTPARSE_FILE = "reports/didntparse_" 
+SUMMARY_FILE = "reports/summary_"
+
