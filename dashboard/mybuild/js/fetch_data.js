03eec92fa633f8fd4fec1a2c24d3d0648298046a
Michal Piekarczyk <namoopsoo>
2017-03-19 14:28:37 -0400

    update doughnut using response data

--- dashboard/mybuild/js/fetch_data.js
+++ dashboard/mybuild/js/fetch_data.js
@@ -10,33 +10,118 @@ var options = { segmentShowStroke : false,
 								animationEasing : 'easeOutCirc'
               }
 
+randomColor = function() {
+	min = 1; max = 255;
+	return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-/* Set the inital data */
-var data = [
-  {
-      value: 1,
-      color:"#e74c3c",
-      highlight: "#c0392b",
-      label: "Red"
-  },
-  {
-      value: 1,
-      color: "#2ecc71",
-      highlight: "#27ae60",
-      label: "Green"
-  },
-  {
-      value: 1,
-      color: "#3498db",
-      highlight: "#2980b9",
-      label: "Blue"
-  }
-];
-
-/* Create the context for applying the chart to the HTML canvas */
-var ctx = $("#graph").get(0).getContext("2d");
+randomRGB = function() {
+	r = randomColor();
+	g = randomColor();
+	b = randomColor();
+	return 'rgba(' + r.toString() + ', ' + g.toString() + ', ' + b.toString() + ', 0.2)';
+}
 
 
-graph = new Chart(ctx).Doughnut(data, options);
+updateDoughnut = function(response) {
+
+	console.log(response);
+	/* Create the context for applying the chart to the HTML canvas */
+	var ctx = $("#graph").get(0).getContext("2d");
+
+	/* Set the inital data */
+	var data = [];
+
+	for (var i = 0; i < response.length; i++) {
+		data.push({
+					value: response[i]['time-length'],
+					// color: "#3498db",
+					color: randomRGB(), // 'rgba(255, 99, 132, 0.2)',
+					// highlight: "#2980b9",
+					label: response[i]['project-identifier']
+				})
+	}
+	console.log('data for doughnut: ');
+	console.log(data);
+
+	// 
+	graph = new Chart(ctx).Doughnut(data, options);
+
+}
+
+
+updateChartWithTimeData = function(parameters) {
+	// Create a new signer
+	var config = {
+		region: 'us-east-1',
+		service: 'execute-api',
+		// AWS IAM credentials, here some temporary credentials
+		accessKeyId: 'AKIAJZRBX47EKWFJQ5UA',
+		secretAccessKey: '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'
+	};
+	console.log("config: ");
+	console.log(config);
+	var signer = new awsSignWeb.AwsSigner(config);
+
+	// Make request url
+	var base_url = 'https://m8fe2knl2f.execute-api.us-east-1.amazonaws.com/staging/summary';
+	var full_uri = base_url + '?end-date=2017-01-03&start-date=2017-01-01&summary-type=%3Acore-category%3Aproject-identifier';
+	// var full_uri = base_url + '?end-date=2017-01-03&start-date=2017-01-01&summary-type=%3Acore-category';
+
+
+
+	// Sign a request
+	var request = {
+		method: 'GET',
+		// URL w/o querystring here.
+		url: base_url,
+		headers: {},
+		params: parameters,
+		data: null
+	};
+	var signed = signer.sign(request);
+
+	// Make Request.
+	$.get({
+		url: full_uri,
+		headers: {
+			'Authorization': signed.Authorization,
+			'Accept': signed.Accept,
+			'x-amz-date': signed['x-amz-date'],
+		},
+
+		success: function( response ) {
+			console.log('success'); // server response
+			console.log( response ); // server response
+
+			updateDoughnut(response);
+		},
+		error: function( response ) {
+			console.log( 'error: ' + response ); // server response
+		}
+	});
+
+
+}
+
+// (function() { })();
+	/*
+	<script src="js/core.js">
+	<script src="js/sha256.js">
+	<script src="js/hmac-sha256.js">
+	<script src="js/aws-sign-web.js">  */
+
+
+
+parameters = {
+	// ?end-date=2017-01-03&start-date=2017-01-01&summary-type=%3Acore-category
+	'end-date': '2017-01-03',
+	'start-date': '2017-01-01',
+	'summary-type': // ':core-category'
+		':core-category:project-identifier'
+}
+
+updateChartWithTimeData(parameters);
+
 
 
