5f8da48698f8c73b9764d7eb6a18b7a410435eea
Michal Piekarczyk <namoopsoo>
2017-07-04 18:58:57 -0400

    cleanup

--- clojure-time/src/one/core.clj
+++ clojure-time/src/one/core.clj
@@ -14,36 +14,6 @@
     [clj-time.coerce :as c]
     ))
 
-(defn sieve-of-eratosthenes
-  "Find all the prime numbers between 2 and <number>"
-  [number]
-  (if (< number 2)
-    (str number " is too low")
-    (let [prime-list (filter #(<= % number) (db/list-primes))
-          largest-prime (if (zero? (count prime-list)) 2 (apply max prime-list))
-          pre-filter (range largest-prime (inc number))]
-      (loop [primes prime-list
-             collection (filter #(not-any? (fn [p] (zero? (mod % p))) primes) pre-filter)]
-        (if-not (seq collection)
-          (vec (apply sorted-set primes))
-          (let [prime (apply min collection)]
-            (db/put-prime (count primes) prime)
-            (recur (conj primes prime)
-                   (remove #(zero? (mod % prime)) collection))))))))
-
-
-; The name of the lambda must include the full namespace in it's name.
-(deflambdafn one.core.lambdafn
-  [in out context]
-  (log/info "Starting Lambda")
-  (let [body (-> in io/reader (json/parse-stream keyword))
-        result (sieve-of-eratosthenes (-> body :max num))]
-    (with-open [w (io/writer out)]
-      (json/generate-stream result w)
-      (log/info "Lambda finished")
-      )
-    )
-  )
 
 (defn vec-strange-trime-them
   "Take a vector of strings and for each, split by ;"
@@ -64,8 +34,6 @@
   )
   
   )
-
-
 
 
 (defn make-time-vec-to-put-dic
@@ -320,6 +288,8 @@
         summaries (summarize-time start-date end-date)
         _ (log/info (str "summaries len: " (count summaries)
                          ", and first is: " (first summaries)))
+        _ (log/info (str "all summaries: " (apply str summaries)
+                         ))
         ]
 
     ; Finally, write the summaries to the summaries table.
